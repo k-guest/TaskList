1cb6eae7a3406e5c131b14986c79e04746212b26
k-guest <k.guest.fr@gmail.com>
2023-01-25 14:27:24 +0100

    Style App Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TaskList from '../TaskList/TaskList';
 
 function App () {
     return (
-        <div className="app">
-            <h1>Task List</h1>
-            <button>New Task</button>
-        </div>
+        <>
+            <div className="app text-center">
+                <h1 className="text-light">Task List</h1>
+                <button className="btn btn-primary">New Task</button>
+            </div>
+            <TaskList />
+        </>
     );
 }
 
---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,11 @@
+import './TaskList.scss';
+
+function TaskList () {
+    return (
+        <div className="task-list">
+            
+        </div>
+    );
+}
+
+export default TaskList;
