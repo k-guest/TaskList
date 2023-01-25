0f104fda64ceade7c5edf6406a4a0cd2ba6ce1f4
k-guest <k.guest.fr@gmail.com>
2023-01-25 13:58:05 +0100

    Update App Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,8 @@ import './App.scss';
 function App () {
     return (
         <div className="app">
-            
+            <h1>Task List</h1>
+            <button>New Task</button>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import '../src/styles/reset.scss';
 
 import { createRoot } from 'react-dom/client';
 
-// import App from './components/App/App';
+import App from './components/App/App';
 
-// const rootReactElement = <App />;
-
-import ReactApp from './components/ReactApp';
-
-const rootReactElement = <ReactApp />;
+const rootReactElement = <App />;
 
 const root = createRoot(document.getElementById('root'));
 
