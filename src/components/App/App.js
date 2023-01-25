fbf4ee74a10eded7cbf215a08a5a1c740d599ac7
k-guest <k.guest.fr@gmail.com>
2023-01-25 15:11:45 +0100

    Creating CreateTask Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,27 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import TaskList from '../TaskList/TaskList';
+import CreateTask from '../CreateTask/CreateTask';
 
 function App () {
+    
+    const [modal, setModal] = useState(false);
+    const toggle = () => {
+        setModal(!modal);
+    }
+
     return (
         <>
             <div className="app text-center">
                 <h1 className="text-light">Task List</h1>
-                <button className="btn btn-primary">New Task</button>
+                <button className="btn btn-primary" onClick={() => setModal(true)}>New Task</button>
             </div>
             <TaskList />
+            <CreateTask
+                modal = {modal}
+                toggle = {toggle}
+            />
         </>
     );
 }
---
+++ src/components/CreateTask/CreateTask.js
@@ -0,0 +1,73 @@
+import './CreateTask.scss';
+import { useState } from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
+
+function CreateTask ({ modal, toggle }) {
+
+    const [taskName, setTaskName] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        
+        if(name === "taskName") {
+            setTaskName(value)
+        } else {
+            setTaskDescription(value)
+        } 
+    }
+
+    return (
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>
+                Create Task
+            </ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail">
+                            Task Name
+                        </Label>
+                        <Input
+                            type="text"
+                            value={taskName}
+                            onChange={handleChange}
+                            name = "taskName"
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="examplePassword">
+                            Description
+                        </Label>
+                        <Input
+                            rows="5"
+                            type="textarea"
+                            value={taskDescription}
+                            onChange={handleChange}
+                            name = "taskDescription"
+                        />
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={toggle}>
+                    Create
+                </Button>{' '}
+                <Button color="secondary" onClick={toggle}>
+                    Cancel
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
+
+export default CreateTask;
